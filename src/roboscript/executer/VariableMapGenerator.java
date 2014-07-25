c1d07c8be86dfdadcd35da32ca80c5c2a4261e7f
Philipp Kuinke <blindeyehalo1@gmail.com>
2014-07-25 16:41:36 +0200

    added additional constants

--- src/roboscript/executer/VariableMapGenerator.java
+++ src/roboscript/executer/VariableMapGenerator.java
@@ -24,10 +24,31 @@ public class VariableMapGenerator {
 			variableMap.put(constants.getProperty("false"), new Number(0));
 			// variables.put("true", new Number(1));
 			// variables.put("false", new Number(0));
-			variableMap.put(constants.getProperty("maximum_number"), new Number(Double.MAX_VALUE));
-			variableMap.put(constants.getProperty("minimum_number"), new Number(Double.MIN_VALUE));
-			variableMap.put(constants.getProperty("random_number"), new RandomNumber());
+			variableMap.put(constants.getProperty("maximum_number"),
+					new Number(Double.MAX_VALUE));
+			variableMap.put(constants.getProperty("minimum_number"),
+					new Number(Double.MIN_VALUE));
+			variableMap.put(constants.getProperty("random_number"),
+					new RandomNumber());
 
+			// Directions
+			/*
+			 * 1 2 3
+			 * 
+			 * 4 0 5
+			 * 
+			 * 6 7 8
+			 */
+
+			variableMap.put(constants.getProperty("stay"), new Number(0));
+			variableMap.put(constants.getProperty("northwest"), new Number(1));
+			variableMap.put(constants.getProperty("north"), new Number(2));
+			variableMap.put(constants.getProperty("northeast"), new Number(3));
+			variableMap.put(constants.getProperty("west"), new Number(4));
+			variableMap.put(constants.getProperty("east"), new Number(5));
+			variableMap.put(constants.getProperty("southwest"), new Number(6));
+			variableMap.put(constants.getProperty("south"), new Number(7));
+			variableMap.put(constants.getProperty("southeast"), new Number(8));
 		}
 		return variableMap;
 
